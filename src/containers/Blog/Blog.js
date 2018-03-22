1e0bc8ea2c7582f9c1b7fe357af2434c5b0d0b27
Momin Tariq <Momin.Tariq@finra.org>
2018-03-22 18:26:42 -0400

    nested routes, lazy routing, route redirection

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import './Blog.css';
-import { Route, Link } from 'react-router-dom';
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+// import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
+
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+});
 
 class Blog extends Component {
+    state = {
+        auth: true
+    }
 
     render () {
         return (
@@ -12,17 +20,30 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to={{
+                            <li><NavLink 
+                                to="/posts/" 
+                                exact
+                                activeClassName="my-active"
+                                activeStyle={{
+                                    color: '#fa923f',
+                                    textDecoration: 'underline'
+                                }}>Posts</NavLink></li>
+                            <li><NavLink to={{
                                 pathname: '/new-post',
                                 hash: '#submit',
                                 search: '?quick-submit=true'
-                            }}>New Post</Link></li>
+                            }}>New Post</NavLink></li>
                         </ul>
                     </nav>
                 </header>
-                <Route path="/" exact component={Posts}/>
-                <Route path="/new-post" component={NewPost}/>
+                
+                <Switch>
+                    {this.state.auth ? <Route path="/new-post" component={AsyncNewPost}/>: null}
+                    <Route path="/posts" component={Posts}/>
+                    <Route render={() => <h1>Not found</h1>}/>
+                    {/*<Redirect from="/" to="/posts" />*/}
+                    {/*<Route path="/" component={Posts}/>*/}
+                </Switch>
             </div>
         );
     }
